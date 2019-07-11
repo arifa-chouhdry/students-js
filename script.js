8c3141cca50a790dc2ff1f7e1b984aa77e3dbe1e
arifa-chouhdry <48832658+arifa-chouhdry@users.noreply.github.com>
2019-07-11 12:18:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let techKaroStudent = [{
+name: "arifa",
+age: 22 ,
+hobbies: {"reading" : "studing"},
+isStudying: true,
+
+} , 
+{
+name: "Sara",
+age: 23 ,
+hobbies: {"coding" : "studing"},
+professions :{ "teaching" : "freeLancer"},
+isTeaching: true,
+}]
+;
